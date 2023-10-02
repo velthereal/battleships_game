efcaeefae0afc935e574d10429603add9d88139e
velthereal <lera2005olex@gmail.com>
2023-10-02 14:50:13 +0300

    formatted

--- js/components/controller.js
+++ js/components/controller.js
@@ -2,33 +2,33 @@ import { Model } from "./model.js";
 import { View } from "./view.js";
 
 export class Controller{
-    constructor(gridElement){
-        this.view = new View(gridElement);
-        this.board = this.view.getBoard();
-        this.data = new Model().getShips();
-    }
-    startGame(){
-        for (let i = 0; i < this.board.length; i++) {
-            for (let j = 0; j < this.board.length; j++) {
-                this.board[i][j].addEventListener('click', () => {
-                    if(this.checkShipFire(i, j)){
-                        this.view.showShips(this.board[i][j]);
-                    }
-                    else {
-                        this.view.showHits(this.board[i][j]);
-                    }
-                })
+   constructor(gridElement){
+      this.view = new View(gridElement);
+      this.board = this.view.getBoard();
+      this.data = new Model().getShips();
+   }
+   startGame(){
+      for (let i = 0; i < this.board.length; i++) {
+         for (let j = 0; j < this.board.length; j++) {
+            this.board[i][j].addEventListener('click', () => {
+					if(this.checkShipFire(i, j)){
+						this.view.showShips(this.board[i][j]);
+					}
+					else {
+						this.view.showHits(this.board[i][j]);
+					}
+            })
+         }
+      }
+   }
+   checkShipFire(x,y){
+      	for (let i = 0; i < this.data.length; i++) {
+         	for (let j = 0; j < this.data[i].location.length; j++) {
+               if(this.data[i].location[j][0] == x && this.data[i].location[j][1] == y){
+                  return true;
+               }
             }
-        }
-    }
-    checkShipFire(x,y){
-        for (let i = 0; i < this.data.length; i++) {
-            for (let j = 0; j < this.data[i].location.length; j++) {
-                if(this.data[i].location[j][0] == x && this.data[i].location[j][1] == y){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+      	}
+      	return false;
+	}
 }
--- js/components/model.js
+++ js/components/model.js
@@ -1,158 +1,157 @@
 // true - horisontal
 // false - vertical
 export class Model{
-    constructor(){
-        this.ships = [
-            {
-                location:[0,0,0,0],
-                hits:['','',''],
-                direction:true,
-            },
-            {
-                location:[0,0,0,],
-                hits:['','',''],
-                direction:true,
-            },
-            {
-                location:[0,0,0,],
-                hits:['','',''],
-                direction:true,
-            },
-            {
-                location:[0,0,],
-                hits:['','',],
-                direction:true,
-            },        
-            {
-                location:[0,0,],
-                hits:['','',],
-                direction:true,
-            }, 
-            {
-                location:[0,0,],
-                hits:['','',],
-                direction:true,
-            },
-            {
-                location:[0,],
-                hits:['',],
-                direction:true,
-            },
-            {
-                location:[0,],
-                hits:['',],
-                direction:true,
-            },
-            {
-                location:[0,],
-                hits:['',],
-                direction:true,
-            },
-            {
-                location:[0,],
-                hits:['',],
-                direction:true,
-            },
-            
-        ];
-        this.hits = 0;
-        this.size = 10;
-        this.generateLocation();
-    }
-    getShips(){
-        return this.ships;
-    }
-    generateLocation(){
-        for (let j = 0; j < this.ships.length;) {
-            let direct = Math.floor(Math.random() * 2) == 0 ? false : true;
-            let x = Math.floor(Math.random() * 10);
-            let y = Math.floor(Math.random() * 10);
-            if(!this.checkShipsCoord(x,y,j) || !this.checkPointer(x,y,j)) continue;
-            this.ships[j].location[0] = [x, y];
-            this.ships[j].direction = direct;
-            let flag = true;
-            for (let i = 1; i < this.ships[j].location.length; i++) {
-                direct ? y++ : x++;
-                if(this.checkOut(x, y) && this.checkShipsCoord(x,y,j) && this.checkPointer(x,y,j)){
-                    this.ships[j].location[i] = [x,y];
-                } 
-                else {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag){
-                this.createPointer(j);
-                j++;
-            }
-        }
-    }
-    checkPointer(x,y, end){
-        for (let i = 0; i < end; i++) {
-            for (let j = 0; j < this.ships[i].pointer.length; j++) {
-                if(this.ships[i].pointer[j][0] == x && this.ships[i].pointer[j][1] == y){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    createPointer(index){
-        let pointer = [];
-        let current = this.ships[index].location;
-        for (let i = 0; i< current.length; i++) {
-            if(this.ships[index].direction){
-                if(i == 0){
-                    pointer.push([current[i][0] - 1, current[i][1]  - 1]);
-                    pointer.push([current[i][0], current[i][1] - 1]);
-                    pointer.push([current[i][0] + 1, current[i][1] - 1]);
-                }
-                if(i == current.length - 1){
-                    pointer.push([current[i][0] - 1, current[i][1] + 1]);
-                    pointer.push([current[i][0], current[i][1]  + 1]);
-                    pointer.push([current[i][0] + 1, current[i][1]  + 1]);
-                }
-                pointer.push([current[i][0] - 1, current[i][1]]);
-                pointer.push([current[i][0] + 1, current[i][1]]);
-            }
+   constructor(){
+      this.ships = [
+         {
+            location:[0,0,0,0],
+            hits:['','',''],
+            direction:true,
+         },
+         {
+            location:[0,0,0,],
+            hits:['','',''],
+            direction:true,
+         },
+         {
+            location:[0,0,0,],
+            hits:['','',''],
+            direction:true,
+         },
+         {
+            location:[0,0,],
+            hits:['','',],
+            direction:true,
+         },        
+         {
+            location:[0,0,],
+            hits:['','',],
+            direction:true,
+         }, 
+         {
+            location:[0,0,],
+            hits:['','',],
+            direction:true,
+         },
+         {
+            location:[0,],
+            hits:['',],
+            direction:true,
+         },
+         {
+            location:[0,],
+            hits:['',],
+            direction:true,
+         },
+         {
+            location:[0,],
+            hits:['',],
+            direction:true,
+         },
+         {
+            location:[0,],
+            hits:['',],
+            direction:true,
+         },   
+      ];
+      this.hits = 0;
+      this.size = 10;
+   	this.generateLocation();
+   }
+   getShips(){
+      return this.ships;
+   }
+   generateLocation(){
+      for (let j = 0; j < this.ships.length;) {
+      	let direct = Math.floor(Math.random() * 2) == 0 ? false : true;
+         let x = Math.floor(Math.random() * 10);
+         let y = Math.floor(Math.random() * 10);
+         if(!this.checkShipsCoord(x,y,j) || !this.checkPointer(x,y,j)) continue;
+         this.ships[j].location[0] = [x, y];
+         this.ships[j].direction = direct;
+         let flag = true;
+         for (let i = 1; i < this.ships[j].location.length; i++) {
+            direct ? y++ : x++;
+            if(this.checkOut(x, y) && this.checkShipsCoord(x,y,j) && this.checkPointer(x,y,j)){
+               this.ships[j].location[i] = [x,y];
+            } 
             else {
-                if(i == 0){
-                    pointer.push([current[i][0] - 1, current[i][1]  - 1]);
-                    pointer.push([current[i][0] - 1, current[i][1]]);
-                    pointer.push([current[i][0] - 1, current[i][1] + 1]);
-                }
-                if(i == current.length - 1){
-                    pointer.push([current[i][0] + 1, current[i][1] - 1]);
-                    pointer.push([current[i][0] + 1, current[i][1]]);
-                    pointer.push([current[i][0] + 1, current[i][1] + 1]);
-                }
-                pointer.push([current[i][0], current[i][1] - 1]);
-                pointer.push([current[i][0], current[i][1] + 1]);
+               flag = false;
+               break;
             }
-        }
-        this.ships[index].pointer = pointer.filter((e) => {
-            return e[0] >= 0 && e[0] < this.size && e[1] >= 0 && e[1] < this.size;
-        });
-    }
-    createPointerAllShips(){
-        for (let i = 0; i < this.ships.length; i++) {
-            this.createPointer(i);
-        }
-    }
-    checkOut(x, y){
-        if(x < 0 || x >= this.size || y < 0 || y >= this.size){
-            return false;
-        }
-        return true;
-    }
-    checkShipsCoord(x, y, end){
-        for (let i = 0; i < end; i++) {
-            for (let j = 0; j < this.ships[i].location.length; j++) {
-                if(this.ships[i].location[j][0] == x && this.ships[i].location[j][1] == y){
-                    return false;
-                }
+         }
+         if(flag){
+            this.createPointer(j);
+            j++;
+         }
+      }
+   }
+   checkPointer(x,y, end){
+      for (let i = 0; i < end; i++) {
+         for (let j = 0; j < this.ships[i].pointer.length; j++) {
+            if(this.ships[i].pointer[j][0] == x && this.ships[i].pointer[j][1] == y){
+               return false;
             }
-        }
-        return true;
-    }
+         }
+      }
+      return true;
+   }
+   createPointer(index){
+      let pointer = [];
+      let current = this.ships[index].location;
+      for (let i = 0; i< current.length; i++) {
+         if(this.ships[index].direction){
+            if(i == 0){
+               pointer.push([current[i][0] - 1, current[i][1]  - 1]);
+               pointer.push([current[i][0], current[i][1] - 1]);
+               pointer.push([current[i][0] + 1, current[i][1] - 1]);
+            }
+            if(i == current.length - 1){
+               pointer.push([current[i][0] - 1, current[i][1] + 1]);
+               pointer.push([current[i][0], current[i][1]  + 1]);
+               pointer.push([current[i][0] + 1, current[i][1]  + 1]);
+            }
+            pointer.push([current[i][0] - 1, current[i][1]]);
+            pointer.push([current[i][0] + 1, current[i][1]]);
+         }
+         else {
+            if(i == 0){
+               pointer.push([current[i][0] - 1, current[i][1]  - 1]);
+               pointer.push([current[i][0] - 1, current[i][1]]);
+               pointer.push([current[i][0] - 1, current[i][1] + 1]);
+            }
+            if(i == current.length - 1){
+               pointer.push([current[i][0] + 1, current[i][1] - 1]);
+               pointer.push([current[i][0] + 1, current[i][1]]);
+               pointer.push([current[i][0] + 1, current[i][1] + 1]);
+            }
+            pointer.push([current[i][0], current[i][1] - 1]);
+            pointer.push([current[i][0], current[i][1] + 1]);
+         }
+      }
+      this.ships[index].pointer = pointer.filter((e) => {
+         return e[0] >= 0 && e[0] < this.size && e[1] >= 0 && e[1] < this.size;
+      });
+   }
+   createPointerAllShips(){
+      for (let i = 0; i < this.ships.length; i++) {
+         this.createPointer(i);
+      }
+   }
+   checkOut(x, y){
+      if(x < 0 || x >= this.size || y < 0 || y >= this.size){
+         return false;
+      }
+      return true;
+   }
+   checkShipsCoord(x, y, end){
+      for (let i = 0; i < end; i++) {
+         for (let j = 0; j < this.ships[i].location.length; j++) {
+            if(this.ships[i].location[j][0] == x && this.ships[i].location[j][1] == y){
+               return false;
+            }
+         }
+		}
+      return true;
+   }
 }
